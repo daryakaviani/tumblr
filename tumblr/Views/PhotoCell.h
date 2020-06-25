b27864d4e7fcd8c8e209af453c6143e9efa60634
Darya Kaviani <dkaviani@fb.com>
2020-06-25 09:52:44 -0700

    Finished

---
+++ tumblr/Views/PhotoCell.h
@@ -0,0 +1,18 @@
+//
+//  PhotoCell.h
+//  tumblr
+//
+//  Created by dkaviani on 6/25/20.
+//  Copyright © 2020 dkaviani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ tumblr/Views/PhotoCell.m
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.m
+//  tumblr
+//
+//  Created by dkaviani on 6/25/20.
+//  Copyright © 2020 dkaviani. All rights reserved.
+//
+
+#import "PhotoCell.h"
+
+@implementation PhotoCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
